938ab7309f34b3a7185f3e669330e979d105a6bf
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-12-07 19:12:51 +0300

    :construction: update requests

--- lib/models/term.rb
+++ lib/models/term.rb
@@ -1,4 +1,5 @@
 require 'mongoid'
+require 'sparql/client'
 
 # Model of technical terms
 class Term
@@ -19,5 +20,74 @@ class Term
       dbpedia_uri: dbpedia_uri,
       en_title: en_title
     }
+  end
+
+  # get rus text from rdfs:comment
+  def upload_rus(sparql)
+    sparql.query(
+      <<-SPARQL
+        SELECT DISTINCT ?concept ?description
+        WHERE {
+          ?concept rdfs:comment ?description .
+          ?concept rdfs:label "#{en_title}"@en .
+          FILTER ( lang(?description) = "ru" )
+        }
+      SPARQL
+    )
+  end
+
+  # get rus text from dbo:abstract
+  def upload_rus_mod(sparql)
+    sparql.query(
+      <<-SPARQL
+        SELECT DISTINCT ?concept ?description
+        WHERE {
+          ?concept dbo:abstract ?description .
+          ?concept rdfs:label "#{en_title}"@en .
+          FILTER ( lang(?description) = "ru" )
+        }
+      SPARQL
+    )
+  end
+
+  # get eng text from rdfs:comment
+  def upload_eng(sparql)
+    sparql.query(
+      <<-SPARQL
+        SELECT DISTINCT ?concept ?description
+        WHERE {
+          ?concept rdfs:comment ?description .
+          ?concept rdfs:label "#{en_title}"@en .
+          FILTER ( lang(?description) = "en" )
+        }
+      SPARQL
+    )
+  end
+
+  # get eng text from dbo:abstract
+  def upload_eng_mod(sparql)
+    sparql.query(
+      <<-SPARQL
+        SELECT DISTINCT ?concept ?description
+        WHERE {
+          ?concept dbo:abstract ?description .
+          ?concept rdfs:label "#{en_title}"@en .
+          FILTER ( lang(?description) = "en" )
+        }
+      SPARQL
+    )
+  end
+
+  def upload!
+    sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
+    solution = upload_rus sparql
+    solution = upload_rus_mod(sparql) if solution.blank? || solution.size < 1 || solution.first[:description].value.size < 50
+    solution = upload_eng(sparql) if solution.blank? || solution.size < 1
+    solution = upload_eng_mod(sparql) if solution.blank? || solution.size < 1 || solution.first[:description].value.size < 50
+    return if solution.blank? || solution.size < 1
+    self.dbpedia_uri = solution.first[:concept].value
+    self.dbpedia_description = solution.first[:description].value
+    self.save!
+    self
   end
 end
