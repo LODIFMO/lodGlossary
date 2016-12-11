128494e479256c0616ee0d5c26369a0ef2b90351
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-12-11 21:04:46 +0300

    :construction: update global model

--- src/Models.elm
+++ src/Models.elm
@@ -1,12 +1,15 @@
 module Models exposing (..)
 
 import Terms.Models exposing(Term)
+import Routing
 
 type alias Model =
   { terms : List Term
+  , route : Routing.Route
   }
 
-initialModel : Model
-initialModel =
+initialModel : Routing.Route -> Model
+initialModel  route =
   { terms = []
+  , route = route
   }
