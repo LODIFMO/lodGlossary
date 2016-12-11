7b5782950e5cb28cdb56a4df445a1fbfe3c01e19
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-12-11 22:32:25 +0300

    :confused: transfer code to view from main

--- src/View.elm
+++ src/View.elm
@@ -1,9 +1,12 @@
-module View exposing (..)
+module Main exposing (..)
 
-import Html exposing (Html, div, text)
+import Html exposing (..)
 import Messages exposing (Msg(..))
 import Models exposing (Model)
+import Terms.Edit
 import Terms.List
+import Terms.Model exposing (TermId)
+import Routing exposing (Route(..))
 
 view : Model -> Html Msg
 view model =
@@ -12,4 +15,32 @@ view model =
 
 page : Model -> Html Msg
 page model =
-  Html.map TermsMsg (Terms.List.view model.terms)
+  case model.route of
+    TermsRoute ->
+      Html.map TermsMsg (Terms.List.view model.terms)
+    
+    TermRoute id ->
+      termEditPage model id
+
+    NotFoundRoute ->
+      notFoundView
+
+termEditPage : Model -> TermId -> Html Msg
+termEditPage model termId =
+  let
+    maybeTerm =
+      model.terms
+        |> List.filter (\term -> term.id == termId)
+        |> List.head
+  in
+    case maybeTerm of
+      Just term ->
+        Html.map TermsMsg (Terms.Edit.view term)
+      
+      Nothing ->
+        notFoundView
+
+notFoundView : Html Msg
+notFoundView =
+  div []
+    [ text "Not found" ]
