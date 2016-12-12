1afd031de4057927d494ee1ae775dc3eb43d316f
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-12-12 11:46:50 +0300

    :lipstick: add detail ui

--- src/Terms/Edit.elm
+++ src/Terms/Edit.elm
@@ -1,7 +1,7 @@
 module Terms.Edit exposing (..)
 
 import Html exposing (..)
--- import Html.Attributes exposing (..)
+import Html.Attributes exposing (..)
 import Terms.Messages exposing (..)
 import Terms.Models exposing (..)
 
@@ -13,5 +13,24 @@ view model =
 
 form : Term -> Html Msg
 form model =
-  div []
-    [ h1 [] [ text "QWE" ] ]
+  div [ class "panel panel-default" ]
+    [ div [ class "panel-heading" ]
+      [ h3 [] [ text model.title ]
+      ]
+    , div [ class "panel-body" ]
+      [ div [ class "panel panel-success" ]
+        [ div [ class "panel-heading" ] [ text "Эталон" ]
+        , div [ class "panel-body" ]
+          [ div [ attribute "style" "text-align: justify" ]
+              [ text model.description ]
+          ]
+        ]
+      , div [ class "panel panel-danger" ]
+        [ div [ class "panel-heading" ] [ text "Выборка DBpedia" ]
+        , div [ class "panel-body" ]
+          [ div [ attribute "style" "text-align: justify" ]
+              [ text model.dbpedia_description ]
+          ]
+        ]
+      ]
+    ]
