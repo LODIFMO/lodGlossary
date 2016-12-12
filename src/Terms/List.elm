d261d0a3e44387d8b0eb68222cd6c363cfa5cda7
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-12-12 10:15:58 +0300

    :lipstick: add detail info button

--- src/Terms/List.elm
+++ src/Terms/List.elm
@@ -1,7 +1,7 @@
 module Terms.List exposing (..)
 
 import Html exposing (Html, div, text, program, table, thead, tbody, tr, td, th,
-                      button, i)
+                      button, i, a)
 import Html.Attributes exposing (..)
 import Terms.Messages exposing (..)
 import Terms.Models exposing (Term)
@@ -33,5 +33,11 @@ termRow term =
     [ td [ attribute "width" "5%" ] [ text term.title ]
     , td [ attribute "width" "45%" ] [ text term.description ]
     , td [ attribute "width" "45%" ] [ text term.dbpedia_description ]
-    , td [ attribute "width" "5%" ] []
+    , td [ attribute "width" "5%" ]
+      [ a [ attribute "href" ( String.append "/#terms/" term.id )
+          , class "btn btn-info"
+          ]
+          [ i [ class "fa fa-fw fa-eye" ] []
+          ]
+      ]
     ]
