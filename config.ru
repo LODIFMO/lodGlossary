b0108b8471a8499ae69e3c393ee1ae9187189e6a
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-12-14 21:03:26 +0300

    :construction: add token auth

--- config.ru
+++ config.ru
@@ -1,14 +1,5 @@
 require './lib/app'
 require './lib/api'
 
-Rack.Builder.new do
-  use Rack::Session::Cookie, secret: '6KZ8I0a0279hvKC5I3u9v27r1Q41iTKb'
-  
-  use Warden::Manager do |manager|
-    manager.default_strategies :password, :basic
-    manager.failure_app = API
-  end
-
-
-  run Rack::Cascade.new [API, Web]
-end
+use Rack::Session::Cookie
+run Rack::Cascade.new [API, Web]
--- lib/api.rb
+++ lib/api.rb
@@ -1,24 +1,35 @@
 require 'grape'
+require 'warden'
 
 require './lib/models/term' 
+
+Warden::Strategies.add(:my_token) do
+  def authenticate!
+    token = env['HTTP_AUTH_TOKEN'] || env['rack.request.query_hash']['AUTH_TOKEN']
+    if token == '7VmN06huIRQ8RcXgOvmTHqLG02W2Yz5k'
+      success!(User.new)
+    else
+      throw :warden
+    end
+  end
+end
 
 class API < Grape::API
   format :json
   prefix :api
 
-  Warden::Manager.serialize_into_session do |user|
-    user.id
-  end
-
-  Warden::Manager.serialize_from_session do |id|
-    User.get(id)
+  use Warden::Manager do |manager|
+    manager.default_strategies :my_token
+    manager.failure_app = lambda {|env| [401,{}, ["Not authorized"]]}
   end
 
   get :terms do
     Term.all.map(&:to_json)
   end
 
-  post :login do
-    env['warden'].authenticate! :password
+  resource :admin do
+    before do
+      env['warden'].authenticate :my_token
+    end
   end
 end
