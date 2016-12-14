045cd67e5dbabb7c58cf12c28ffceb43a645a164
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-12-14 22:14:53 +0300

    :construction: add on save message

--- src/Users/Messages.elm
+++ src/Users/Messages.elm
@@ -1,5 +1,9 @@
 module Users.Messages exposing (..)
 
+import Http
+import Users.Models exposing (User)
+
 type Msg
   = FormEmail String
   | FormPassword String
+  | OnSave (Result Http.Error User)
